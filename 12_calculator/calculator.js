863c9ef1bf6071446d7e8bc90478536d83504770
Ashish Chaudhary <201731753+AshishChaudhary01@users.noreply.github.com>
2025-05-31 13:23:25 +0545

    Complete exercise 12(calculator)

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  if(arr != null){
+    arr.forEach((value)=>{sum+=value});
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  arr.forEach((value) => {product = product*value});
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+  for(let i=num; i>0; i--){
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
