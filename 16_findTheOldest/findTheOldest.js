b01ee1848dd741d77a984cdc20b7651a388f483e
Ashish Chaudhary <201731753+AshishChaudhary01@users.noreply.github.com>
2025-05-31 16:41:18 +0545

    Complete exercise 16(findTheOldest)

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentDate = new Date().getFullYear();
+  people.forEach(person => {
+    if ("yearOfDeath" in person) {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    }else{
+      person.age = currentDate - person.yearOfBirth;
+    }
+  });
 
+  const oldest = people.reduce(
+    (old, current) => 
+      (old && old.age > current.age) ? old : current
+  );
+  return oldest;
 };
 
 // Do not edit below this line
