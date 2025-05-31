5601df9c3e89ed835d40410b7447fc393c000b98
Ashish Chaudhary <201731753+AshishChaudhary01@users.noreply.github.com>
2025-05-31 14:24:28 +0545

    Complete exercise 14(fibonacci)

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(upTo) {
+  let first = 1;
+  let second = 1;
+  let nth = 0;
 
+  if(upTo<0){ return "OOPS"};
+  if(upTo>0 && upTo<3){ return 1;}
+  for(let counter=2; counter<upTo; counter++){
+    nth = first + second;
+    first = second;
+    second = nth;
+  }
+  return nth;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
