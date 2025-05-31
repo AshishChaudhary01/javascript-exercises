8f2fc0a13ce342cc5962cc3e9115d30dd3cd7c6e
Ashish Chaudhary <201731753+AshishChaudhary01@users.noreply.github.com>
2025-05-31 13:57:16 +0545

    Complete exercise 13(palindromes)

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let revString = "";
+  let palindrome = false;
 
+  //Remove any unwanted character that aren't letters or numbers
+  cleanedString = string
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase();
+  //Reverse the string
+  revString = cleanedString.split("").reverse().join("");
+  if (revString ==cleanedString) {
+    palindrome = true;
+  }
+  return palindrome;
 };
 
 // Do not edit below this line
